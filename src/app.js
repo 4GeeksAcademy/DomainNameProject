c9d14d1d26dc7d73e6a36a8978387f1e73ebd56b
adiaz980 <141368651+adiaz980@users.noreply.github.com>
2023-10-03 22:03:31 +0000

    Final Changes

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".org", ".net", ".us", ".io"];
+
+  let domainNames = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let domain of domains) {
+          domainNames.push(`${pronoun}${adj}${noun}${domain}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join("")} </ul>`;
 };
